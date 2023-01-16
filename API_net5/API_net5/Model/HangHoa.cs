7ac5250e6f5aa701d0a5f6b0e2d8cf171a0d7bd3
Tran-Hoc <58226341+Tran-Hoc@users.noreply.github.com>
2023-01-16 10:33:49 +0700

    03, create table chi tiet don hang and use fluent api https://www.entityframeworktutorial.net/efcore/fluent-api-in-entity-framework-core.aspx

---
+++ API_net5/API_net5/Model/HangHoa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace API_net5.Model
+{
+    public class HangHoaVM
+    {
+        public string TenHang { get; set; }
+        public double DonGia { get; set; }
+    }
+    public class HangHoa : HangHoaVM
+    {
+        public Guid MaHang { get; set; }
+
+    }
+}
