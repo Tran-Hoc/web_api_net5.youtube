a4d6f6d61cc80604e4f5cb646b7b2af2aa7aa49c
Tran-Hoc <58226341+Tran-Hoc@users.noreply.github.com>
2023-01-16 10:33:49 +0700

    04_ controller_return type

---
+++ API_net5/API_net5/Controllers/LoaisController.cs
@@ -0,0 +1,107 @@
+﻿using API_net5.Data;
+using API_net5.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Runtime;
+
+namespace API_net5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoaisController : ControllerBase
+    {
+        private readonly MyDbContext _context;
+        public LoaisController(MyDbContext myDbContext)
+        {
+            _context = myDbContext;
+
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                var dsLoai = _context.Loais.ToList();
+                return Ok(dsLoai);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var loai = _context.Loais.SingleOrDefault(l => l.MaLoai == id);
+            if (loai != null)
+            {
+                return Ok(loai);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+        [Authorize]
+        [HttpPost]
+        public IActionResult CreateNew(LoaiModel loaiModel)
+        {
+            try
+            {
+                var loai = new Loai { TenLoai = loaiModel.TenLoai };
+                _context.Add(loai);
+                _context.SaveChanges();
+
+                return StatusCode(StatusCodes.Status201Created, loai);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateLoaiById(int id, LoaiModel loaiModel)
+        {
+            var loai = _context.Loais.SingleOrDefault(l => l.MaLoai == id);
+
+            try
+            {
+                if (loai != null)
+                {
+                    loai.TenLoai = loaiModel.TenLoai;
+                    _context.SaveChanges();
+
+                    return NoContent();
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteLoaiById(int id)
+        {
+            var loai = _context.Loais.SingleOrDefault(l => l.MaLoai == id);
+            if (loai != null)
+            {
+                _context.Remove(loai);  
+                _context.SaveChanges();
+                return StatusCode(StatusCodes.Status200OK);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+    }
+}
---
+++ API_net5/API_net5/Data/HangHoa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API_net5.Data
+{
+    [Table("HangHoa")]
+    public class HangHoa
+    {
+        [Key]
+        public Guid MaHangHoa { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string  TenHangHoa { get; set; }
+        public string  Mota { get; set; }
+        [Range(0, double.MaxValue)]
+        public double DonGia { get; set; }
+        public byte GiamGia { get; set; }
+
+        public int? MaLoai { get; set; }
+        [ForeignKey("MaLoai")]
+        public Loai Loai { get; set; }
+
+        public ICollection<ChiTietDonHang> chiTietDonHangs { get; set; }
+        public HangHoa()
+        {
+            chiTietDonHangs = new List<ChiTietDonHang>();
+        }
+    }
+}
